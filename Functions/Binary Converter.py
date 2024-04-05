c65ad5bf11ba1992905ad2c025b7f0eb15590b08
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-05 23:29:13 +0300

    Update Binary Converter.py

--- Functions/Binary Converter.py
+++ Functions/Binary Converter.py
@@ -1,29 +1,16 @@
 print("Binary Number Converter")
 
-decimal = 0
-binary = ""
+def dec_to_bin(num):
+    num = bin(num)[2:]
+    return num
 
-for i in range(20000):
-    type = int(input("Convert from Decimal(1) or Binary(0)? "))
-    if type in [0, 1]:
-        break
-    else:
-        print("Please input a value of 1 or 0")
-
-if type == 1:
-    decimal = int(input("Enter a decimal number: "))
-    binary = bin(decimal)[2:]
-elif type == 0:
-    binary = input("Enter a binary number: ")
-
-if type == 1:
-    pass
-elif type == 0:
-    decimal = int(binary, 2)
-
-print("Decimal number:", decimal)
-print("Binary number:", binary)
-
-input("Press Enter to exit...")
+def bin_to_dec(num):
+    num = str(num)
+    num = int(num, 2)
+    return num
     
+num = 1001
 
+num = bin_to_dec(num)
+
+print(num)

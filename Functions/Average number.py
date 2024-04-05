b3f73c11a1135585db2897262c9fbce91fc02a90
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-05 23:16:32 +0300

    Improved Average Number Function
    The function now returns the average number instead of printing it

--- Functions/Average number.py
+++ Functions/Average number.py
@@ -16,7 +16,40 @@ print("Welcome to the average calculator!/n"
       "type 'done' to get the result.")
 
 print("Input your numbers: ")
+while inputing:n = []
+inputing = True
+def calculate_average(n):
+    lgth = len(n)
+    a = 0
+    for i in range(0, lgth):
+        a += n[i]
+    try:
+        a /= lgth
+        return a
+    except ZeroDivisionError:
+        print("You didn't input any numbers.")
+
+print("Welcome to the average calculator!/n"
+      "If write all of your numbers, then/n"
+      "type 'done' to get the result.")
+
+print("Input your numbers: ")
 while inputing:
+    answer = 0
+    answer = input()
+    answer = answer.lower()
+    if answer == "done":
+        inputing = False
+    else:
+        try:
+            answer = int(answer)
+            n.append(answer)
+        except ValueError:
+            pass
+    
+n = calculate_average(n)
+
+print(n)
     answer = 0
     answer = input()
     answer = answer.lower()

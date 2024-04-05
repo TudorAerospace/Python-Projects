55a9ba83a8fecdf65b3799bedf416170deba678e
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-05 22:23:21 +0300

    Update Ascending-Descending.py
    Updated the Ascending-Descending Function. Now, it sorts the entire vector in ascending order, then sorts the last half in descending order.

--- Functions/Ascending-Descending.py
+++ Functions/Ascending-Descending.py
@@ -1,31 +1,49 @@
-print("Sort a list descending then ascending")
-
 import numpy as np 
+
+print("Sort a list descending then ascending")
 
 n = int(input("How many items are in your list? "))
 lst1 = [0] * n
 
-for x in range(n):
-    lst1[x] = int(input("Enter list item {}: ".format(x)))
+try:
+    for x in range(n):
+        lst1[x] = int(input("Enter list item {}: ".format(x)))
+except ValueError:
+    print("Please input only numbers and try again.")
+    for x in range(n):
+        lst1[x] = int(input("Enter list item {}: ".format(x)))
 
 v = np.array(lst1)
 
-lght = len(v)
+def ascending_descending(v):
+    #first order the entire vector in ascending order
 
-# Sort the first half of the array in ascending order
-for j in range(int(lght / 2)):
-    for i in range(int(lght / 2 - j - 1)):
-        if v[i] > v[i + 1]:
-            aux = v[i]
-            v[i] = v[i + 1]
-            v[i + 1] = aux
+    lght = len(v)
+    for j in range(int(lght)):
+        for i in range(int(lght - j - 1)):
+            if v[i] > v[i + 1]:
+                aux = v[i]
+                v[i] = v[i + 1]
+                v[i + 1] = aux
 
-# Sort the second half of the array in descending order
-for j in range(int(lght / 2), lght - 1):
-    for i in range(int(lght / 2), lght - 1):
-        if v[i] < v[i + 1]:
-            aux = v[i]
-            v[i] = v[i + 1]
-            v[i + 1] = aux
+    # Sort the first half of the array in ascending order
+    for j in range(int(lght / 2)):
+        for i in range(int(lght / 2 - j - 1)):
+            if v[i] > v[i + 1]:
+                aux = v[i]
+                v[i] = v[i + 1]
+                v[i + 1] = aux
+
+    # Sort the second half of the array in descending order
+    for j in range(int(lght / 2), lght - 1):
+        for i in range(int(lght / 2), lght - 1):
+            if v[i] < v[i + 1]:
+                aux = v[i]
+                v[i] = v[i + 1]
+                v[i + 1] = aux
+
+    return(v)
+
+v = ascending_descending(v)
 
 print(v)

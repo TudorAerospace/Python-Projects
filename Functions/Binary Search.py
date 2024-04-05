e47eee25ce75e640fa7c31d3683c66f7235147cd
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-05 23:31:39 +0300

    Update Binary Search.py
    Updated Binary Search to be a function

--- Functions/Binary Search.py
+++ Functions/Binary Search.py
@@ -1,36 +1,43 @@
 v = [1, 7, 7, 3, 5, 6, 9, 5, 4]
-found = False
 
-if len(v) == 0:
-    n = int(input("Enter the number of elements: "))
+def binary_search(v):
+    found = False
 
-    print("Please input the elements of your vector: \n")
+    if len(v) == 0:
+        n = int(input("Enter the number of elements: "))
+
+        print("Please input the elements of your vector: \n")
+        for i in range(n):
+            a = int(input())
+            v.append(a)
+
+    n = len(v)
     for i in range(n):
-        a = int(input())
-        v.append(a)
+        for j in range(n):
+            if v[i] < v[j]:
+                aux = v[j]
+                v[j] = v[i]
+                v[i] = aux
 
-n = len(v)
-for i in range(n):
-    for j in range(n):
-        if v[i] < v[j]:
-            aux = v[j]
-            v[j] = v[i]
-            v[i] = aux
+    x = int(input("What number would you like to search for? "))
+    st = 0
+    en = n
 
-x = int(input("What number would you like to search for? "))
-st = 0
-en = n
+    while st <= en and found == False:
+        mid = int((st + en)/2)
+        if v[mid] == x:
+            found = True
+        if v[mid] < x:
+            st = mid + 1
+        if v[mid] > x:
+            en = mid - 1
 
-while st <= en and found == False:
-    mid = int((st + en)/2)
-    if v[mid] == x:
-        found = True
-    if v[mid] < x:
-        st = mid + 1
-    if v[mid] > x:
-        en = mid - 1
+    if found == True:
+        print(f"YES {v[mid]} is on position {mid}")
+        return mid
+    else:
+        print("NO")
 
-if found == True:
-    print(f"YES {v[mid]} is on position {mid}")
-else:
-    print("NO")
+v = binary_search(v)
+
+print(v)

bfbb4ba693aa5c08cb0232c74130c7ee3d7ca2fb
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-08 18:32:09 +0300

    Update Divisor Finder.py
    Turned the divisor finder program into a function. Also added user input error handling.

--- Functions/Divisor Finder.py
+++ Functions/Divisor Finder.py
@@ -1,15 +1,25 @@
 print("Divisor Finder")
 
-number = int(input("Please enter your number: "))
+type_ = False
+while not type_:
+    try:
+        num = int(input("Please enter your number: "))
+        type_ = True
+    except ValueError:
+        print("Please input an integer")
 
-is_prime = True
+def divisor_finder(num):
+    is_prime = True
+    divisors = []
+    for x in range(2, num):
+        if num % x == 0:
+            is_prime = False
+            divisors.append(x)
+    if is_prime:
+        return None
+    else:
+        return divisors
 
-for x in range(2, number):
-    if number % x == 0:
-        is_prime = False
-        print(x)
-
-if is_prime == True:
-  print("Your number is prime")
+print(divisor_finder(num))
 
 input("Press Enter to exit...")

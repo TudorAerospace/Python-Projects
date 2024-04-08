cbe13beb9d090e50550cf72ae38c10e7ac129042
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-08 15:59:42 +0300

    Updated Collatz Conjecture.py
    Updated the logic to be inside a function

--- Functions/Collatz Conjecture.py
+++ Functions/Collatz Conjecture.py
@@ -5,17 +5,17 @@ print("Collatz Conjecture Solver")
 
 number = int(input("Enter a number: "))
 
-while number != 1:
-   if number%2==0:
-      number = number/2
-      print("Number: " , number)
-      steps = steps + 1
-   elif number%2 == 1:
-      number = number * 3 + 1
-      print("Number: " , number)
-      steps = steps + 1
+def collatz(number):
+   while number != 1:
+      if number%2==0:
+         number = number/2
+         print("Number: " , number)
+         steps = steps + 1
+      elif number%2 == 1:
+         number = number * 3 + 1
+         print("Number: " , number)
+         steps = steps + 1
 
-print("Number of steps: " , steps)
-print("Number: " , number)
+   return steps
 
 input("Press Enter to exit...")

71bcc5068b5f91b5a4fc765f2e48f8ed6e469e0b
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-08 18:21:02 +0300

    Update 40.py
    Improved my solution to exercise 40

--- Practice Python/40.py
+++ Practice Python/40.py
@@ -7,7 +7,8 @@ while True:
 	guess = 0
 	while guess not in range(1,10):
 		guess = int(input("Guess a number between 1 and 9: "))
-	number_of_guesses += 1
+		if guess in range(1, 10):
+			number_of_guesses += 1
 	if guess == number:
 		break
 print(f"You needed {number_of_guesses} guesses to guess the number {number}")

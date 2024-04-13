34f1b99504226104ddd046bf8bcf1741f2787700
Tudor Filip <tudorfilip2110@gmail.com>
2024-04-13 12:09:50 +0300

    Uploaded Recursive Consonants Counter.py
    A function that recursively finds the consonants in a string

---
+++ Recursive Consonants Counter.py
@@ -0,0 +1,12 @@
+#Recursive Consonant Counter
+
+def consonants(string, cons_count, n):
+    string = list(string)
+    if string is None or n == len(string):
+        return cons_count
+    elif string[n] not in ["a", "e", "i", "o", "u"]:
+        cons_count += 1
+    return consonants(string, cons_count, n+1)
+
+string = "coco"
+print(consonants(string, 0, 0))
